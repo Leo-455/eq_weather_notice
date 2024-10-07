07d842f351dc8589a17b867989fb9d87a0f12826
Leo_455 <Leo_455@163.com>
2024-10-07 15:15:26 +0800

    修改time变量名防止报错，发生网络中断时程序能继续请求直到网络恢复

--- CENC_eq_notice.py
+++ CENC_eq_notice.py
@@ -28,15 +28,24 @@ wait = 60 #初始化等待时间
 while active == True:
     md5_new = md5 #重置MD5值
     while md5_new == md5: #循环，判断MD5值是否改变
-        sleep(wait)
-        #CENC API
-        url = "https://api.wolfx.jp/cenc_eqlist.json"
-        r = requests.get(url)
-        response = r.json()
-        md5 = response["md5"] #提取新MD5值
-        t = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-        print(f"{t} BJT  {count}次  状态码{r.status_code}")
-        count = count+1
+        try:
+            sleep(wait)
+            #CENC API
+            url = "https://api.wolfx.jp/cenc_eqlist.json"
+            r = requests.get(url)
+            if r.status_code != 200:
+                print("请求失败，状态码:", r.status_code)
+                break  # 可以选择在请求失败时退出
+            response = r.json()
+            md5 = response["md5"] #提取新MD5值
+            t = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+            print(f"{t} BJT  {count}次  状态码{r.status_code}")
+            count = count+1
+        except requests.exceptions.ConnectionError as e:
+            t = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+            print(f"{t} 网络连接中断，正在重试...")
+            sleep(wait)  # 等待后重试
+            continue  # 继续循环，尝试再次请求
     else:
         #sleep(10) #!!调试的时候防止死循环!!
         md5 = response["md5"] #提取新的MD5值
@@ -50,7 +59,7 @@ while active == True:
             depth = response[f"No{number}"]["depth"] #震源深
             intensity = response[f"No{number}"]["intensity"] #最大烈度
             type = response[f"No{number}"]["type"] #自动/正式
-            time = response[f"No{number}"]["time"] #发震时刻
+            eq_time = response[f"No{number}"]["time"] #发震时刻
 
         if type == "reviewed":
             类别 = "正式测定"
@@ -63,8 +72,8 @@ while active == True:
         #Windows通知弹窗
         notification.notify(
             title=f"CENC地震信息（{类别}）",
-            message=f"发震时间：{time},震源:{location}，震级:M{mag}，震源深:{depth}km，最大烈度:{intensity}度",#这玩意咋换行啊？？？
+            message=f"发震时间：{eq_time},震源:{location}，震级:M{mag}，震源深:{depth}km，最大烈度:{intensity}度",#这玩意咋换行啊？？？
             timeout=60, #弹窗持续时间
             )
         #终端输出
-        print(f"CENC地震信息（{类别}）震源:{location}，震级:M{mag}，震源深:{depth}km，最大烈度:{intensity}度")
+        print(f"CENC地震信息（{类别}）,发震时间：{eq_time},震源:{location}，震级:M{mag}，震源深:{depth}km，最大烈度:{intensity}度")
