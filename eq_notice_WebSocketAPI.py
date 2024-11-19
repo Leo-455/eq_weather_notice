bb2049b0465632d9228b780d9158f97db684d3bd
Leo_455 <Leo_455@163.com>
2024-11-19 12:46:17 +0800

    加入CWA预警接收

--- eq_notice_WebSocketAPI.py
+++ eq_notice_WebSocketAPI.py
@@ -33,14 +33,14 @@ def on_message(ws, r):
 
     #中国地震台网 地震信息
     if type == "cenc_eqlist":
-        mag = response[f"{type}"][f"No{number}"]["magnitude"] #震级
-        location = response[f"{type}"][f"No{number}"]["location"] #震源地
-        lon = response[f"{type}"][f"No{number}"]["longitude"] #经度
-        lat = response[f"{type}"][f"No{number}"]["latitude"] #纬度
-        depth = response[f"{type}"][f"No{number}"]["depth"] #震源深
-        intensity = response[f"{type}"][f"No{number}"]["intensity"] #最大烈度
-        eq_time = response[f"{type}"][f"No{number}"]["time"] #发震时刻
-        type = response[f"{type}"][f"No{number}"]["type"] #自动/正式 ！！这行一定要放在最后！！
+        mag = response[f"No{number}"]["magnitude"] #震级
+        location = response[f"No{number}"]["location"] #震源地
+        lon = response[f"No{number}"]["longitude"] #经度
+        lat = response[f"No{number}"]["latitude"] #纬度
+        depth = response[f"No{number}"]["depth"] #震源深
+        intensity = response[f"No{number}"]["intensity"] #最大烈度
+        eq_time = response[f"No{number}"]["time"] #发震时刻
+        type = response[f"No{number}"]["type"] #自动/正式
         
         #格式化输出
         if type == "reviewed":
@@ -53,19 +53,35 @@ def on_message(ws, r):
 
     #JMA 地震情報
     if type == "query_jmaeqlist":
-        mag = response[f"{type}"][f"No{number}"]["magnitude"] #震级
-        location = response[f"{type}"][f"No{number}"]["location"] #震源地
-        lon = response[f"{type}"][f"No{number}"]["longitude"] #经度
-        lat = response[f"{type}"][f"No{number}"]["latitude"] #纬度
-        depth = response[f"{type}"][f"No{number}"]["depth"] #震源深
-        shindo = response[f"{type}"][f"No{number}"]["shindo"] #最大震度
-        eq_time = response[f"{type}"][f"No{number}"]["time_full"] #发震时刻
-        tsunami = response[f"{type}"][f"No{number}"]["info"] #海啸信息
+        mag = response[f"No{number}"]["magnitude"] #震级
+        location = response[f"No{number}"]["location"] #震源地
+        lon = response[f"No{number}"]["longitude"] #经度
+        lat = response[f"No{number}"]["latitude"] #纬度
+        depth = response[f"No{number}"]["depth"] #震源深
+        shindo = response[f"No{number}"]["shindo"] #最大震度
+        eq_time = response[f"No{number}"]["time_full"] #发震时刻
+        tsunami = response[f"No{number}"]["info"] #海啸信息
 
         #格式化输出
         type = "JMA 地震情報"
         output = f"发震时间：{eq_time},震源:{location}（{lat},{lon}），震级:M{mag}，震源深:{depth}，最大震度:{shindo}，{tsunami}"
         message(output,type) #调用通知函数
+
+    #CWA 地震预警
+    if type == "cwa_eew":
+        eq_time = response["OriginTime"] #发震时间
+        location = response["HypoCenter"] #震源地
+        report_num = response["ReportNum"] #EEW发报数
+        lon = response["Longitude"] #震源地经度
+        lat = response["Latitude"] #震源地纬度
+        mag = response["Magunitude"] #震级
+        depth = response["Depth"] #震源深度
+        intensity = response["MaxIntensity"] #最大震度
+
+        #格式化输出
+        type = f"CWA 地震预警（第{report_num}报）"
+        output = f"发震时间：{eq_time}，震源:{location}（{lat},{lon}），震级:M{mag}，震源深:{depth}km，最大震度:{intensity}"
+        message(output,type)
 
 # 回调函数，处理连接关闭
 def on_close(ws, close_status_code, close_msg):
@@ -87,12 +103,12 @@ def message(output,type):
     toaster = ToastNotifier()
     toaster.show_toast(f"{type}",f"{output}",duration=10)
     #终端输出
-    print(f"{type}，{output}")
-#    #写入文件
-#    with open("D:\\programing\\python\\eq_weather_notice\\eq_log.txt.txt","a",encoding='utf-8') as file:
-#        file.write(f"{type}，{output}\n")
-#    #打开记录
-#    subprocess.call(["python","D:\\programing\\python\\eq_weather_notice\\realtime_sindo.py"])
+    print(f"{type}：{output}")
+    #写入文件
+    with open("D:\\programing\\python\\eq_weather_notice\\eq_log.txt.txt","a",encoding='utf-8') as file:
+        file.write(f"{type}：{output}\n")
+    #打开记录
+    subprocess.call(["python","D:\\programing\\python\\eq_weather_notice\\realtime_sindo.py"])
 
 # WebSocket 客户端线程函数
 def start_websocket():
